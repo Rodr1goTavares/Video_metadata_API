32a0542a2be6fa84ac2a7b45effe9ce0303e18c5
Rodr1goTavares <rodr1gowzxz2@gmail.com>
2025-03-09 10:19:43 -0300

    :construction: File handler added.

---
+++ internal/handlers/fileHandler.go
@@ -0,0 +1,25 @@
+package internal
+
+import (
+	"fmt"
+	"net/http"
+)
+
+
+func postVideo(writer http.ResponseWriter, request *http.Request) {
+  if request.Method != "POST" {
+    http.Error(writer, "Method not allowed", http.StatusMethodNotAllowed)
+    return
+  }
+
+  request.ParseMultipartForm(10 << 20) // 10 MB
+
+  file, handler, err := request.FormFile("video")
+  if err != nil {
+    http.Error(writer, "Error to get file", http.StatusBadRequest)
+    return
+  }
+  defer file.Close()
+
+  fmt.Printf("File name: %s\n", handler.Filename)
+}
--- pkg/videoMetadataService.go
+++ pkg/videoMetadataService.go
@@ -1,5 +1,11 @@
 package pkg
 
 type VideoMetadata struct {
-
+  size int64
+  duration float32
+  height int16
+  width int16
+  aspectRatio string
 }
+
+
